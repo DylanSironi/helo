28f599505a83e2df1c342ad063cdabed31b00c1b
DylanSironi <sironidylan@gmail.com>
2020-10-09 16:33:01 -0500

    work

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
 class Auth extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            authView: false
+        }
+    }
+
+    // componentDidMount(){
+    //     if(this.props.user.username){
+    //         this.props.history.push('/dashboard');
+    //     }
+    // }
+
+    // handleInput = (event) => {
+    //     this.setState({[event.target.name]: event.target.value})
+    // }
+
+    // handleToggle = () => {
+    //     this.setState({authView: !this.state.authView})
+    // }
+
     render() {
         return (
             <div>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Link, withRouter} from "react-router-dom";
 
 class Nav extends Component {
     render() {
+        // console.log(this.props)
         return (
-            <div>
-                Nav
+            <div className= 'nav'>
+                <img src='https://robohash.org/7IC.png?set=set2&size=150x150' alt='robot'/>
+                <button >Home</button>
+                <button>New Post</button>
+                <button>Logout</button>
+
+                
+                 {this.props.location.pathname !== '/'
+            ? (<nav>
+                <Link to='/dashboard' className='nav-links'></Link>
+               </nav>)
+            : null}
             </div>
         )
     }
 }
-export default Nav
+export default withRouter(Nav)
